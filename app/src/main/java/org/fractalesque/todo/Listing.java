77a86147a8e325d37e6d7e21c3290a75b07a1fda
Félix Gagnon-Grenier <fractalesque@gmail.com>
2017-08-04 16:30:42 -0400

    getView basic override

--- app/src/main/java/org/fractalesque/todo/Listing.java
+++ app/src/main/java/org/fractalesque/todo/Listing.java
@@ -5,14 +5,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.content.Context;
-import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class Listing extends AppCompatActivity {
@@ -73,13 +71,27 @@ public class Listing extends AppCompatActivity {
 
     private class MyAdapter extends ArrayAdapter<Task> {
 
-        MyAdapter(Context context, int textViewRessourceId, Task[] elems) {
-            super(context, textViewRessourceId, elems);
+        private final Context context;
+        private final Task[] tasks;
+
+        MyAdapter(Context context, int textViewRessourceId, Task[] tasks) {
+            super(context, textViewRessourceId, tasks);
+            this.context = context;
+            this.tasks = tasks;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return super.getView(position, convertView, parent);
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(android.R.layout.simple_list_item_2, parent, false);
+                TextView title = convertView.findViewById(android.R.id.text1);
+                TextView description = convertView.findViewById(android.R.id.text2);
+                title.setText(this.tasks[position].getTitle());
+                description.setText(this.tasks[position].getDescription());
+            }
+            return convertView;
         }
     }
 
